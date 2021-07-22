ce91783c35572566e2db1d50b0ec8cc25cf0c8e3
roznerx <ignaciorosner@gmail.com>
2021-07-21 22:54:21 -0300

    Project finished, including extras except for keyboard support, which I will implement next

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+let n1 = "";
+let n2 = "";
+let operator = "";
+
+let enabler = "OFF";
+
 const add = (n1, n2) => n1 + n2;
 const substract = (n1, n2) => n1 - n2;
 const multiply = (n1, n2) => n1 * n2;
@@ -12,110 +18,227 @@ const operate = (operator, n1, n2) => {
 //display
 
 const display = document.getElementById("display");
-let displayValue = display.value;
-
-displayValue = 0;
+display.value = "0";
 
 //numbers
 
 const sevenBtn = document.querySelector("#seven");
 sevenBtn.addEventListener("click", () => {
-    display.value += 7;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "7";
+        enabler = "OFF";
+    } else {
+        display.value += "7";
+    }
 });
 
 const eightBtn = document.querySelector("#eight");
 eightBtn.addEventListener("click", () => {
-    display.value += 8;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "8"; 
+        enabler = "OFF"; 
+    } else {
+        display.value += "8";
+    }
 });
 
 const nineBtn = document.querySelector("#nine");
 nineBtn.addEventListener("click", () => {
-    display.value += 9;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "9";  
+        enabler = "OFF";
+    } else {
+        display.value += "9";
+    }
 });
 
 const fourBtn = document.querySelector("#four");
 fourBtn.addEventListener("click", () => {
-    display.value += 4;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "4";  
+        enabler = "OFF";
+    } else {
+        display.value += "4";
+    }
 });
 
 const fiveBtn = document.querySelector("#five");
 fiveBtn.addEventListener("click", () => {
-    display.value += 5;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "5";  
+        enabler = "OFF";
+    } else {
+        display.value += "5";
+    }
 });
 
 const sixBtn = document.querySelector("#six");
 sixBtn.addEventListener("click", () => {
-    display.value += 6;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "6";  
+        enabler = "OFF";
+    } else {
+        display.value += "6";
+    }
 });
 
 const threeBtn = document.querySelector("#three");
 threeBtn.addEventListener("click", () => {
-    display.value += 3;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "3"; 
+        enabler = "OFF"; 
+    } else {
+        display.value += "3";
+    }
 });
 
 const twoBtn = document.querySelector("#two");
 twoBtn.addEventListener("click", () => {
-    display.value += 2;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "2";  
+        enabler = "OFF";
+    } else {
+        display.value += "2";
+    }
 });
 
 const oneBtn = document.querySelector("#one");
 oneBtn.addEventListener("click", () => {
-    display.value += 1;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "1";  
+        enabler = "OFF";
+    } else {
+        display.value += "1";
+    }
 });
 
 const zeroBtn = document.querySelector("#zero");
 zeroBtn.addEventListener("click", () => {
-    display.value += 0;
+    if (display.value === "0" || display.value == "ERROR!" || enabler == "ON") {
+        display.value = "0";
+        enabler = "OFF";
+    } else {
+        display.value += "0";
+    }
 });
 
 //operations
 
 const addBtn = document.querySelector("#add");
 addBtn.addEventListener("click", () => {
-    n1 = parseFloat(display.value);
-    display.value = 0;   
-    operator = add;
+
+    if (typeof operator == "function") {
+        if (operator === divide && n2 === "0") {
+            display.value = "ERROR!";
+            operator = "";
+        } else {
+            n2 = parseFloat(display.value);
+            display.value = Math.round((operate(operator, n1, n2)) * 100 + Number.EPSILON) / 100;
+            n1 = parseFloat(display.value);
+            operator = add;
+            enabler = "ON";
+        }
+    } else {
+        n1 = parseFloat(display.value);
+        display.value = "0";   
+        operator = add;
+        n2 = display.value;
+    }
 });
 
 const substractBtn = document.querySelector("#substract");
 substractBtn.addEventListener("click", () => {
-    n1 = parseFloat(display.value);
-    display.value = 0;   
-    operator = substract;
+    if (typeof operator == "function") {
+        if (operator === divide && n2 === "0") {
+            display.value = "ERROR!";
+            operator = "";
+        } else {
+            n2 = parseFloat(display.value);
+            display.value = Math.round((operate(operator, n1, n2)) * 100 + Number.EPSILON) / 100;
+            n1 = parseFloat(display.value);
+            operator = substract;
+            enabler = "ON";  
+        }
+    } else {
+        n1 = parseFloat(display.value);
+        display.value = "0";   
+        operator = substract;
+        n2 = display.value;
+    }
 });
 
 const multiplyBtn = document.querySelector("#multiply");
 multiplyBtn.addEventListener("click", () => {
-    n1 = parseFloat(display.value);
-    display.value = 0;   
-    operator = multiply;
+    if (typeof operator == "function") {
+        if (operator === divide && n2 === "0") {
+            display.value = "ERROR!";
+            operator = "";
+        } else {
+            n2 = parseFloat(display.value);
+            display.value = Math.round((operate(operator, n1, n2)) * 100 + Number.EPSILON) / 100;
+            n1 = parseFloat(display.value);
+            operator = multiply;     
+            enabler = "ON";   
+        }
+    } else {
+        n1 = parseFloat(display.value);
+        display.value = "0";   
+        operator = multiply;
+        n2 = display.value;
+    }
 });
 
 const divideBtn = document.querySelector("#divide");
 divideBtn.addEventListener("click", () => {
-    n1 = parseFloat(display.value);
-    display.value = 0;   
-    operator = divide;
+    if (typeof operator == "function") {
+        if (operator === divide && n2 === "0") {
+            display.value = "ERROR!";
+            operator = "";
+        } else {
+            n2 = parseFloat(display.value);
+            display.value = Math.round((operate(operator, n1, n2)) * 100 + Number.EPSILON) / 100;
+            n1 = parseFloat(display.value);
+            operator = divide;    
+            enabler = "ON";   
+        } 
+    } else {
+        n1 = parseFloat(display.value);
+        display.value = "0";   
+        operator = divide;
+        n2 = display.value;
+    }
 });
 
 //equal
 
 const equalBtn = document.querySelector("#equal");
-equalBtn.addEventListener("click", () => {
-    n2 = parseFloat(display.value);
-    display.value = operate(operator, n1, n2);
-    displayValue = n1;
+equalBtn.addEventListener("click", () => {   
+        n2 = parseFloat(display.value);
+        if (operator === divide && n2 === 0) {
+            display.value = "ERROR!";
+            operator = "";
+        } else {
+            display.value = Math.round((operate(operator, n1, n2)) * 100 + Number.EPSILON) / 100;
+            n1 = display.value;
+            operator = "";
+        }
 });
 
 //point
 
 const pointBtn = document.querySelector("#point");
+pointBtn.addEventListener("click", () => {
+    display.value += ".";
+});
 
 //clear and delete
 
 const clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", () => {
-    display.value = 0;
+    display.value = "0";
+    n1 = "";
+    n2 = "";
+    operator = "";
 });
 
 const deleteBtn = document.querySelector("#delete");
@@ -123,5 +246,6 @@ deleteBtn.addEventListener("click", () => {
     displayValue = display.value;
     newDisplayValue = displayValue.substr(0, displayValue.length -1);
     display.value = newDisplayValue;
-    DisplayValue = display.value;
+    displayValue = display.value;
 });
+
